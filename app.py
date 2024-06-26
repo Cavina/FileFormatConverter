7e1e3bc30f2145747da061d838ac6c9763762ccb
Chris Avina <chrisavina@ChrissMacStudio.attlocal.net>
2024-06-26 13:48:20 -0700

    Fixed Syntax error with Raw Strings for Reg_eX

--- app.py
+++ app.py
@@ -23,20 +23,15 @@ Schema example:
 '''
 
 
-'''
-def get_column_names -> list[str]
-    Inputs -> A JSON schema, a dataset name, sorting key
-    Outputs -> A list of the column names as strings
-Retrieves the column names from a JSON schema.
-Sorts based on the key passed.
-'''
+
 def get_column_names(schemas, ds_name, sorting_key='column_position') -> list[str]:
     column_details = schemas[ds_name]
     columns = sorted(column_details, key=lambda col: col[sorting_key])
     return [col['column_name'] for col in columns]
 
 def read_csv(file, schemas) -> pd.DataFrame:
-    file_path_list = re.split('[/\\\]'. file)
+    reg_ex = r'[\\/]'
+    file_path_list = re.split(reg_ex, file)
     ds_name = file_path_list[-2]
     file_name = file_path_list[-1]
     columns = get_column_names(schemas, ds_name)
@@ -57,7 +52,8 @@ def file_converter(src_base_dir, tgt_base_dir, ds_name):
 
     for file in files:
         df = read_csv(file, schemas)
-        file_name = re.split('[/\\\]', file)[-1]
+        reg_ex = r'[\\/]'
+        file_name = re.split(reg_ex, file)[-1]
         to_json(df, tgt_base_dir, ds_name, file_name)
 
 
